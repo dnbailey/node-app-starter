90fa7cefcc4eb149cf304da91b8a1226d472fd98
Nathan Bailey <dev@dnbailey.org>
2017-11-12 17:37:12 -0500

    Add express server and intitial route

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+app.set('port', process.env.PORT || 3000)
+
+app.use('/', require('./routes/index.js'))
+
+app.listen(app.get('port'), () => {
+  console.log('Listening on ' + app.get('port'))
+})
